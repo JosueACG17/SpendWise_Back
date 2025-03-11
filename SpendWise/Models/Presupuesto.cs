e8b0ed8225d1fb96b7099b351d6e6193a652fcd5
MikeInsano <snowiestax72@gmail.com>
2025-03-11 09:25:04 -0500

    Agregar las propiedades para decimal de Gasto y Presupuesto

--- SpendWise/Models/Presupuesto.cs
+++ SpendWise/Models/Presupuesto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace SpendWise.Models
 {
@@ -8,7 +9,10 @@ namespace SpendWise.Models
         public int Id { get; set; }
         public int UsuarioId { get; set; }
         public int CategoriaId { get; set; }
+
+        [Precision(18, 2)]
         public decimal Monto { get; set; }
+        
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
         public Usuario Usuario { get; set; }
